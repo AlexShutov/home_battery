52fb13fb5c71565de2c2e92c1c0cecf06d0d725b
shutov.aleksey9 shutov.aleksey9 <shutov.sleksey9@rwb.ru>
2026-09-11 07:32:10 +0300

    изменил тип входной ножки, чтобы был включен подтягивающий резистор, нужно теперь переключатель включать на землю, перепаять

--- src/time_switcher.cpp
+++ src/time_switcher.cpp
@@ -4,11 +4,12 @@ const uint8_t TimeSwitcher::PIN_MIDDLE_PERIOD;
 const uint8_t TimeSwitcher::PIN_EXPENSIVE_PERIOD;
 
 void TimeSwitcher::init() {
-  pinMode(PIN_MIDDLE_PERIOD, INPUT);
-  pinMode(PIN_EXPENSIVE_PERIOD, INPUT);
+  pinMode(PIN_MIDDLE_PERIOD, INPUT_PULLUP);
+  pinMode(PIN_EXPENSIVE_PERIOD, INPUT_PULLUP);
 }
 
+// Активность периода определяется уровнем LOW (входы подтянуты на высокий уровень).
 void TimeSwitcher::getState(TimeSwitcherState& out) {
-  out.isMiddlePeriodActive = (digitalRead(PIN_MIDDLE_PERIOD) == HIGH);
-  out.isExpensivePeriodActive = (digitalRead(PIN_EXPENSIVE_PERIOD) == HIGH);
+  out.isMiddlePeriodActive = (digitalRead(PIN_MIDDLE_PERIOD) == LOW);
+  out.isExpensivePeriodActive = (digitalRead(PIN_EXPENSIVE_PERIOD) == LOW);
 }
